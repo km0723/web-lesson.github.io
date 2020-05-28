e6ce91655fb78d1eb9afc36fe9456e798e0fb257
km0723 <58931148+km0723@users.noreply.github.com>
2020-05-28 14:43:48 +0900

    【初級】NodeJS 勉強時の成果物
    ### インストールしたもの
      * node
      * npm
      * homebrew
      * mysql
    
    ###必要なパッケージ(nodejs)
      * npm install express
      * npm install ejs
      * npm install nodemon
      * npm install mysql
    
    migrate.sql の実行が必要。

---
+++ progate_lesson/nodejs_lesson/migrate.sql
@@ -0,0 +1,4 @@
+CREATE TABLE items (`id` int auto_increment, `name` text, primary key (id));
+insert into items (name) values ( 'じゃがいも' );
+insert into items (name) values ( 'にんじん' );
+insert into items (name) values ( 'たまねぎ' );
